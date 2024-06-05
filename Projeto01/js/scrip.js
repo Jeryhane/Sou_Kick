38104cf75fd7d75aa33524d00d8d2f2d87f60117
Jeryhane <jeryhanemvalente@yahoo.com.br>
2024-06-04 22:31:28 -0300

    ajustes projeto01 home carrosel e contato

--- Projeto01/js/scrip.js
+++ Projeto01/js/scrip.js
@@ -20,3 +20,23 @@ $(document).on('click', '#btnEmail', function (e) {
 
     }
 });
+let slideIndex = 0;
+
+showSlides();
+
+function showSlides() {
+  let i;
+  var slides = $(".mySlides");
+  var dots = $(".dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].css("display", "none");  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
